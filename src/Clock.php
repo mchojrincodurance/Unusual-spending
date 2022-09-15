900f79900e5e8dce9a96f2e8d2f25425e6e3fffe
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-15 15:26:11 +0200

    Clock tested

--- src/Clock.php
+++ src/Clock.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 class Clock
 {
+    private int $currentMonth;
 
     public function __construct()
     {
+        $this->currentMonth = 0;
     }
 
     public function nextMonth(): void
     {
-
+        $this->currentMonth++;
     }
 
     public function getCurrentMonth(): int
     {
-        return 0;
+        return $this->currentMonth;
     }
 }
--- test/unit/ClockShould.php
+++ test/unit/ClockShould.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-
 use PHPUnit\Framework\TestCase;
 
 class ClockShould extends TestCase
