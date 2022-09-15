457782192c7b1cb25e07573b3a49c538937bc6c2
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-15 16:09:49 +0200

    Real implementation passing the test

--- src/PaymentRepository.php
+++ src/PaymentRepository.php
@@ -4,24 +4,26 @@ declare(strict_types=1);
 
 class PaymentRepository
 {
+    private array $payments;
+    private Clock $clock;
 
     public function __construct(Clock $clock)
     {
+        $this->payments = [];
+        $this->clock = $clock;
     }
 
     public function addPayment(UserId $userId, Payment $payment): self
     {
+        $this->payments[$this->clock->getCurrentMonth()][$userId->getValue()][] = $payment;
+        
         return $this;
     }
 
     public function getUserMonthlyPayments(UserId $userId, int $month): array
     {
-        return [
-            new Payment(
-                new Price(1.5),
-                new PaymentDescription("A payment"),
-                Category::Restaurants
-            ),
-        ];
+        return array_key_exists($month, $this->payments) ? 
+            array_key_exists($userId->getValue(), $this->payments[$month]) ? $this->payments[$month][$userId->getValue()] : [] 
+            : [];
     }
 }
