9a78eb1003d44dd61642e6f6ae45da9b48f1e045
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-15 16:04:00 +0200

    Added parameterized tests to make the test fail

--- test/unit/PaymentRepositoryTest.php
+++ test/unit/PaymentRepositoryTest.php
@@ -9,25 +9,29 @@ class PaymentRepositoryTest extends TestCase
 {
     /**
      * @test
+     * @dataProvider paymentsProvider
      */
-    public function store_payments(): void
+    public function store_payments(array $payments): void
     {
         $currentMonth = 0;
         $userId = new UserId(0);
 
         $paymentRepository = new PaymentRepository(new Clock());
 
-        $firstPayment = new Payment(
-            new Price(1.5),
-            new PaymentDescription("A payment"),
-            Category::Restaurants
-        );
+        foreach ($payments as $payment) {
+            $paymentRepository->addPayment($userId, $payment);
+        }
 
-        $monthlyPayments = [$firstPayment,];
+        $this->assertEquals($payments, $paymentRepository->getUserMonthlyPayments($userId, $currentMonth));
+    }
 
-        $paymentRepository
-            ->addPayment($userId, $firstPayment);
-
-        $this->assertEquals($monthlyPayments, $paymentRepository->getUserMonthlyPayments($userId, $currentMonth));
+    /**
+     * @throws NegativePriceException
+     */
+    public function paymentsProvider() : array
+    {
+        return [
+            [ [new Payment(new Price(10.2), new PaymentDescription('A dinner'), Category::Restaurants ), new Payment(new Price(50.6), new PaymentDescription('A bike ride'), Category::Entertainment )] ]
+        ];
     }
 }
