156f0d30aa68195bdc7732c453f0eac59e7282c2
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-15 11:29:31 +0200

    A more meaningfull failing test

--- test/TriggerUnusualSpendingEmailShould.php
+++ test/TriggerUnusualSpendingEmailShould.php
@@ -8,20 +8,22 @@ class TriggerUnusualSpendingEmailShould extends MockeryTestCase
 {
     private Clock $clock;
     private PaymentRepository $paymentRepository;
+    private EmailSender $emailSenderSpy;
+    private TriggerUnusualSpendingEmail $triggerUnusualSpendingEmail;
 
     /**
      * @test
      */
-    public function send_emails(): void
+    public function sent_email_with_expected_body_and_subject(): void
     {
-        $this->buildBaseScenario();
+        $this->triggerUnusualSpendingEmail->trigger(1);
 
-        $emailSender = Mockery::spy(EmailSender::class);
-        $triggerUnusualSpendingEmail = new TriggerUnusualSpendingEmail($emailSender);
-        $triggerUnusualSpendingEmail->trigger(1);
-
-        $emailSender
-            ->shouldHaveReceived('send')
+        $this->emailSenderSpy
+            ->shouldHaveReceived('send', [
+                    $this->buildExpectedSubject(),
+                    $this->buildExpectedBody()
+                ]
+            )
             ->once();
     }
 
@@ -80,17 +82,39 @@ class TriggerUnusualSpendingEmailShould extends MockeryTestCase
             ->addPayment(
                 $userId,
                 new Payment(
-                    new Price(8.20),
+                    new Price(18.20),
                     new PaymentDescription("Bike ride"),
                     Category::Entertainment
                 )
             );
     }
 
-    protected function setUp() : void
+    protected function setUp(): void
     {
         parent::setUp();
         $this->clock = new Clock(new DateTimeImmutable());
         $this->paymentRepository = new PaymentRepository($this->clock);
+        $this->emailSenderSpy = Mockery::spy(EmailSender::class);
+        $this->triggerUnusualSpendingEmail = new TriggerUnusualSpendingEmail($this->emailSenderSpy);
+    }
+
+    private function buildExpectedBody(): string
+    {
+        return <<<EOT
+Hello card user!
+
+We have detected unusually high spending on your card in these categories:
+
+* You spent $100.00 on restaurants
+
+Love,
+
+The Credit Card Company
+EOT;
+    }
+
+    private function buildExpectedSubject(): string
+    {
+        return "Unusual spending of $118.20 detected!";
     }
 }
