5e36283e41d10f2530f579de69c7c568e780b854
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-15 15:24:48 +0200

    First failing clock test

--- test/unit/ClockShould.php
+++ test/unit/ClockShould.php
@@ -7,5 +7,14 @@ use PHPUnit\Framework\TestCase;
 
 class ClockShould extends TestCase
 {
-
+    /**
+     * @test
+     */
+    public function not_skip_months(): void
+    {
+        $clock = new Clock();
+        $previousMonth = $clock->getCurrentMonth();
+        $clock->nextMonth();
+        $this->assertEquals($previousMonth + 1, $clock->getCurrentMonth());
+    }
 }
