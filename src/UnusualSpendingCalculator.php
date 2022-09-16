6f99065691aa3c0c4ec69e4aacd7e66250f96082
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-16 10:56:28 +0200

    Fake implementation passing the test

--- src/UnusualSpendingCalculator.php
+++ src/UnusualSpendingCalculator.php
@@ -8,6 +8,8 @@ class UnusualSpendingCalculator
     public function getUnusualSpending(int $userId, int $getCurrentMonth, int $getPreviousMonth): array
     {
         return [
+            Category::Restaurants->name => 25,
+            Category::Entertainment->name => 150,
         ];
     }
 }
